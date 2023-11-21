d7c240413c6014e5b6b958a8e37580ff2e2ae676
rido-min <rido-min@ghnouser.com>
2023-11-20 22:52:04 -0800

    use latest SDKLite

--- MqttTester/Program.cs
+++ MqttTester/Program.cs
@@ -1,32 +1,25 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Azure.IoTMQ.IoTHubConnector.Client;
+using Microsoft.Azure.IoTMQ.IoTHubConnector.Client.Connection;
+using Microsoft.Azure.IoTMQ.IoTHubConnector.Client.Connection.Settings;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MQTTnet;
 using MQTTnet.Client;
-using MQTTnet.Extensions.MultiCloud.AzureIoTClient;
-using MQTTnet.Extensions.MultiCloud.BrokerIoTClient;
-using MQTTnet.Extensions.MultiCloud.Connections;
 using System.Diagnostics;
 
 Trace.Listeners.Add(new ConsoleTraceListener());
 
 using IHost host = Host.CreateDefaultBuilder(args).Build();
 host.Start();
+
 IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
 ILogger logger = host.Services.GetRequiredService<ILogger<Program>>();
 
-var cs = new ConnectionSettings(config.GetConnectionString("cs")!);
+var cs = MqttConnectionSettings.FromConnectionString(config.GetConnectionString("Default")!);
 logger.LogWarning("Connecting with: {cs}", cs);
-IMqttClient mqttClient;
-
-if (!string.IsNullOrEmpty(cs.IdScope) || cs.HostName!.Contains("azure-devices.net"))
-{
-    logger.LogWarning("Nuget: {n}", HubDpsFactory.NuGetPackageVersion);
-    mqttClient = await HubDpsFactory.CreateFromConnectionSettingsAsync(cs);
-}
-else
-{
-    logger.LogWarning("Nuget: {n}", BrokerClientFactory.NuGetPackageVersion);
-    mqttClient = await BrokerClientFactory.CreateFromConnectionSettingsAsync(cs);
-}
+logger.LogWarning("Nuget: {n}", DeviceClient.GetSdkVersion());
+IMqttClient mqttClient = new MqttFactory().CreateMqttClient(MqttNetTraceLogger.CreateTraceLogger());
+await mqttClient.ConnectAsync(new MqttClientOptionsBuilder().WithMqttConnectionSettings(cs).Build());
 logger.LogWarning("Client Connected: {c}", mqttClient.IsConnected);
