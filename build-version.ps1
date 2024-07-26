8b826c6b553233ac2e2c8b0c95abd884e65171b1
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2024-07-26 13:12:50 -0700

    pack as a tool

--- build-version.ps1
+++ build-version.ps1
@@ -1,3 +1,3 @@
 $version=(nbgv get-version -v NuGetPackageVersion)
-dotnet pack MqttTester\MqttTester.csproj
+dotnet pack -c Release MqttTester\MqttTester.csproj
 dotnet publish /t:PublishContainer .\MqttTester\ --os linux --arch x64 /p:ContainerImageTags=$version /p:ContainerRegistry="ghcr.io" /p:ContainerRepository="ridomin/mqtttester"
